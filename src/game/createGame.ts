a55e930c9df80e6230da7597b11a0e95cd22df63
Chris Horne <cdh612@gmail.com>
2026-09-22 22:55:49 -0600

    refactor: gaps in maps/travel; harvest on HUD; no install hooks

--- src/game/createGame.ts
+++ src/game/createGame.ts
@@ -2,13 +2,9 @@ import * as Phaser from "phaser";
 import { GAME_H, GAME_W, type JobId, type MapId } from "./content";
 import { BootScene } from "./scenes/BootScene";
 import { WorldScene } from "./scenes/WorldScene";
-import { installSolids } from "./installSolids";
 import { registerKinds } from "./world/kinds";
-import { installPiles } from "./world/install";
 
 registerKinds();
-installPiles(WorldScene);
-installSolids(WorldScene);
 
 export type CreateOpts = {
   parent: HTMLElement;
